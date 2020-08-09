e766cb84f54d16d39d8fc34531c0d665118ed6d7
snowzhao <snow@snowzhaodeMacBook-Pro.local>
2020-08-09 09:21:14 +0800

    update abi

--- contracts/MyToken.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.6.2;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-
-contract MyToken is ERC20 {
-    constructor() public ERC20("My Token", "MYT") {
-        _mint(msg.sender, 1000);
-    }
-}
